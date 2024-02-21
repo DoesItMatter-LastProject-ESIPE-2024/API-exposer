26d0b240a274ce184819d20862b987cccbf520c1
Ornlu14 <logan.beaury@free.fr>
2024-02-21 18:19:52 +0100

    update: modification du serveur (ajout de ninja, swagger dynamic, modification des routes)

--- __main__.py
+++ __main__.py
@@ -13,22 +13,49 @@ import json
 import os
 
 from waitress import serve  # production server
-from flask import Flask  # web python server
-from swagger_ui import api_doc # open api
+from flask import Flask, render_template, request  # web python server
+from swagger_ui.core import ApplicationDocument # open api
+from swagger_ui import api_doc
+from jinja2 import Environment, FileSystemLoader, select_autoescape # config template
 
 
 app = Flask(__name__)
+
+environment = Environment(
+    loader=FileSystemLoader(searchpath="./"),
+    autoescape=select_autoescape()
+)
 
 working_dir = os.path.dirname(os.path.abspath(__file__))
 conf_path = os.path.join(working_dir, 'config/swagger.yml')
 
 api_doc(app, config_path=conf_path, url_prefix='/api/doc', title='API DIM')
 
+@app.route('/doc')
+def devices_menu():
+    """ Road to display devices id from list and select them """
+    server_ip=request.headers.get("Host").split(':')[0]
+    html_template=environment.get_template("ressources/devices.html")
+    
+    return render_template(html_template, devices=[1,2,3], server_ip=f"{server_ip}", port="8080")
+    
 
-@app.route('/')
-def hello_world():
-    """ TODO """
-    return "<h1> Hello World! </h1>"
+@app.route('/doc/')
+@app.route('/doc/<id>')
+def display_swagger(id):
+    """ Road to display the swagger API of a devices """
+    server_ip=request.headers.get("Host").split(':')[0]
+    """ TODO methode to retrieve cluster_path for swagger.yml """
+    cluster_paths = ["A", "B", "C"] 
+    
+    swagger_template=environment.get_template("config/swagger.yml")
+    config_string=swagger_template.render(server_ip=f"{server_ip}", paths=cluster_paths)
+    
+    print(config_string)
+    
+    api_doc=ApplicationDocument(None, config_spec=config_string, url_prefix="/api", title="API DIM")
+    
+    return api_doc.doc_html
 
 
 @app.route('/test')
@@ -41,4 +68,4 @@ def display_random_list():
 
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=8080)
+    serve(app, host="0.0.0.0", port=8080)
