9842893b3d7859d9f2c0b42587396e458865d7fd
Brian BABILLON <brian.babillon@wattearth.com>
2024-02-22 14:04:11 +0100

    add swagger as static resource

--- __main__.py
+++ __main__.py
@@ -8,61 +8,64 @@ It displays dynamically OpenApi 3.0 with swagger-ui-py (23.9.23)
 =============================================================================
 """
 
-from dynamic_render import register_new_renderer # render open api dynamic
+from dynamic_render import register_new_renderer  # render open api dynamic
 import random
 import json
 import os
 
 from waitress import serve  # production server
 from flask import Flask, redirect, render_template, request  # web python server
-from swagger_ui.core import ApplicationDocument # open api
+from swagger_ui.core import ApplicationDocument  # open api
 from swagger_ui import api_doc
-from jinja2 import Environment, FileSystemLoader, select_autoescape # config template
+from jinja2 import Environment, FileSystemLoader, select_autoescape  # config template
 
+working_dir = os.path.dirname(os.path.abspath(__file__))
+conf_path = os.path.join(working_dir, 'config/swagger.yml')
+static_folder_path = os.path.join(working_dir, 'static')
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=static_folder_path)
 
 environment = Environment(
     loader=FileSystemLoader(searchpath="./"),
     autoescape=select_autoescape()
 )
 
-working_dir = os.path.dirname(os.path.abspath(__file__))
-conf_path = os.path.join(working_dir, 'config/swagger.yml')
+api_renderer = register_new_renderer(app, "/swagger/blueprint")
+print(app.static_url_path)
 
-api_doc(app, config_path=conf_path, url_prefix='/api/doc', title='API DIM')
-api_renderer=register_new_renderer(app, "/swagger/blueprint")
 
 @app.route('/')
 def redirect_internal():
     return redirect("/html/devices", code=302)
 
+
 @app.route('/html/devices')
 def devices_menu():
     """ Road to display devices id from list and select them """
-    server_ip=request.headers.get("Host").split(':')[0]
-    html_template=environment.get_template("ressources/devices.html")
-    
-    return render_template(html_template, devices=[1,2,3], server_ip=f"{server_ip}", port="8080")
-    
+    server_ip = request.headers.get("Host").split(':')[0]
+    html_template = environment.get_template("ressources/devices.html")
+
+    return render_template(html_template, devices=[1, 2, 3], server_ip=f"{server_ip}", port="8080")
+
 
 @app.route('/api/doc/')
 @app.route('/api/doc/<id>')
 def display_swagger(id):
     """ Road to display the swagger API of a devices """
-    server_ip=request.headers.get("Host").split(':')[0]
-    """ TODO methode to retrieve cluster_path for swagger.yml """
-    cluster_paths = ["A", "B", "C"] 
-    
-    swagger_template=environment.get_template("config/swagger.yml")
-    config_string=swagger_template.render(server_ip=f"{server_ip}", paths=cluster_paths)
-    
+    server_ip = request.headers.get("Host").split(':')[0]
+    # """ TODO methode to retrieve cluster_path for swagger.yml """
+    cluster_paths = ["A", "B", "C"]
+
+    swagger_template = environment.get_template("config/swagger.yml")
+    config_string = swagger_template.render(
+        server_ip=f"{server_ip}", paths=cluster_paths)
+
     print(config_string)
-    
-    
-    #api_doc=dynamic_render.ApplicationDocument(None, config_spec=config_string, url_prefix="/api/v0", title="API DIM")
-    
+
+    # api_doc=dynamic_render.ApplicationDocument(None, config_spec=config_string, url_prefix="/api/v0", title="API DIM")
+
     return api_renderer.from_string(config_spec=config_string)
+
 
 @app.route('/test')
 def display_random_list():
