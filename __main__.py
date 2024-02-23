707fd5c2de86cb2cbe62ed3788785ae44e73c78f
Brian BABILLON <brian.babillon@wattearth.com>
2024-02-23 12:28:11 +0100

    moved script in main function

--- __main__.py
+++ __main__.py
@@ -18,71 +18,70 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape  # config te
 
 from dynamic_render import register_new_renderer  # render open api dynamic
 
-working_dir = os.path.dirname(os.path.abspath(__file__))
-conf_path = os.path.join(working_dir, 'config/swagger.yml')
-static_folder_path = os.path.join(working_dir, 'static')
 
-name = __name__.split('.', maxsplit=1)[0]
-app = Flask(name, static_folder=static_folder_path)
+def main():
+    working_dir = os.path.dirname(os.path.abspath(__file__))
+    conf_path = os.path.join(working_dir, 'config/swagger.yml')
+    static_folder_path = os.path.join(working_dir, 'static')
 
-environment = Environment(
-    loader=FileSystemLoader(searchpath="./"),
-    autoescape=select_autoescape()
-)
+    name = __name__.split('.', maxsplit=1)[0]
+    app = Flask(name, static_folder=static_folder_path)
 
-api_renderer = register_new_renderer(app, "/swagger/blueprint")
-print(app.static_url_path)
-
-SWAGGER_PATH = 'html/swagger'
-
-
-@app.route('/')
-def redirect_internal():
-    """Redirects index to html/devices"""
-    return redirect("/html/devices", code=302)
-
-
-@app.route('/html/devices')
-def devices_menu():
-    """Returns a html menu composed of device's id from list"""
-    server_ip = request.headers.get("Host").split(':')[0]
-    html_template = environment.get_template("dynamic/devices.html")
-
-    return render_template(
-        html_template,
-        devices=[1, 2, 3],
-        server_ip=f"{server_ip}",
-        port="8080",
-        swagger_path=SWAGGER_PATH
+    environment = Environment(
+        loader=FileSystemLoader(searchpath="./"),
+        autoescape=select_autoescape()
     )
 
+    api_renderer = register_new_renderer(app, "/swagger/blueprint")
+    print(app.static_url_path)
 
-@app.route(f'/{SWAGGER_PATH}/<id>')
-def swagger_ui(node_id: int):
-    """Dynamically renders a swagger ui with the correct documentation"""
-    return api_renderer.from_string(node_api_documentation(node_id))
+    SWAGGER_PATH = 'html/swagger'
 
+    @app.route('/')
+    def redirect_internal():
+        """Redirects index to html/devices"""
+        return redirect("/html/devices", code=302)
 
-@app.route('/api/doc/<id>')
-def node_api_documentation(node_id: int) -> str:
-    """Returns an OpenAPI documentation in yaml format for a matter node"""
-    server_ip = request.headers.get("Host").split(':')[0]
+    @app.route('/html/devices')
+    def devices_menu():
+        """Returns a html menu composed of device's id from list"""
+        server_ip = request.headers.get("Host").split(':')[0]
+        html_template = environment.get_template("dynamic/devices.html")
 
-    # cluster_paths = ["A", "B", "C"]
-    cluster_paths = []
+        return render_template(
+            html_template,
+            devices=[1, 2, 3],
+            server_ip=f"{server_ip}",
+            port="8080",
+            swagger_path=SWAGGER_PATH
+        )
 
-    swagger_template = environment.get_template("config/swagger.yml")
-    return swagger_template.render({
-        "server_ip": server_ip,
-        "paths": cluster_paths
-    })
+    @app.route(f'/{SWAGGER_PATH}/<id>')
+    def swagger_ui(node_id: int):
+        """Dynamically renders a swagger ui with the correct documentation"""
+        return api_renderer.from_string(node_api_documentation(node_id))
 
+    @app.route('/api/doc/<id>')
+    def node_api_documentation(node_id: int) -> str:
+        """Returns an OpenAPI documentation in yaml format for a matter node"""
+        server_ip = request.headers.get("Host").split(':')[0]
 
-@app.route('/test')
-def display_random_list():
-    """Returns a json list of 5 random number between 1,10 in json format"""
-    return json.dumps([random.randint(1, 10) for _ in range(0, 5)])
+        # cluster_paths = ["A", "B", "C"]
+        cluster_paths = []
+
+        swagger_template = environment.get_template("config/swagger.yml")
+        return swagger_template.render({
+            "server_ip": server_ip,
+            "paths": cluster_paths
+        })
+
+    @app.route('/test')
+    def display_random_list():
+        """Returns a json list of 5 random number between 1,10 in json format"""
+        return json.dumps([random.randint(1, 10) for _ in range(0, 5)])
+
+    serve(app, host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=8080)
+    main()
