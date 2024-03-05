c4a8fd8f211cad8cbd3ab4a3b3ad008a9de2c6ce
Brian BABILLON <brian.babillon@wattearth.com>
2024-03-05 12:19:32 +0100

    fix field naming in my_client

--- api_exposer/my_client.py
+++ api_exposer/my_client.py
@@ -21,9 +21,9 @@ class MyClient:
     """
 
     def __init__(self, url: str):
-        self.url: str = url
         self.nodes: Dict[int, MatterNode] = {}
-        self._client_global: Optional[MatterClient] = None
+        self._url: str = url
+        self._client: Optional[MatterClient] = None
         self._wait_listening: Event = Event()
         self._task: Optional[Task] = None
 
@@ -53,18 +53,18 @@ class MyClient:
 
     async def _run_client(self):
         async with ClientSession() as session:
-            async with MatterClient(self.url, session) as client:
-                self._client_global = client
-                self._client_global.subscribe_events(self._handle_event)
+            async with MatterClient(self._url, session) as client:
+                self._client = client
+                self._client.subscribe_events(self._handle_event)
 
                 # start listening
-                await self._client_global.start_listening(self._wait_listening)
+                await self._client.start_listening(self._wait_listening)
 
     async def _get_nodes(self):
         await self._wait_listening.wait()
         self.nodes.update({
             node.node_id: node
-            for node in self._client_global.get_nodes()
+            for node in self._client.get_nodes()
         })
         logging.debug(self.nodes)
 
@@ -85,7 +85,7 @@ class MyClient:
 
     async def send_cluster_command(self, node_id: int, endpoint_id: int, command: ClusterCommand):
         """Sends a cluster command to an endpoint of a matter node"""
-        return await self._client_global.send_device_command(
+        return await self._client.send_device_command(
             node_id,
             endpoint_id,
             command,
@@ -93,14 +93,14 @@ class MyClient:
 
     async def read_cluster_attribute(self, node_id: int, endpoint_id: int, cluster_id: int, attribute_id: int) -> Any:
         """TODO"""
-        return await self._client_global.read_attribute(
+        return await self._client.read_attribute(
             node_id,
             f'{endpoint_id}/{cluster_id}/{attribute_id}'
         )
 
     async def write_cluster_attribute(self, node_id: int, endpoint_id: int, cluster_id: int, attribute_id: int, value: Any) -> Any:
         """TODO"""
-        return await self._client_global.write_attribute(
+        return await self._client.write_attribute(
             node_id,
             f'{endpoint_id}/{cluster_id}/{attribute_id}',
             value
